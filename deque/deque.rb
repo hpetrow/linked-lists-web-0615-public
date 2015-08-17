502663b8afd4c5f5ebf75e271b7784e1a6999cc2
hpetrow <heather.petrow@gmail.com>
2015-08-17 16:08:40 -0400

    done

--- deque/deque.rb
+++ deque/deque.rb
@@ -1,5 +1,56 @@
 require_relative '../simple-lists/simple_linked_list'
 
 class Deque
+  attr_accessor :list
 
+  def initialize
+    @list = nil
+  end
+
+  def push(datum)
+    if !self.list
+      self.list = Element.new(datum, nil)
+    elsif !self.list.next
+      self.list.next = Element.new(datum, self.list)
+    else
+      self.list.previous.next = Element.new(datum, self.list)
+    end
+  end
+
+  def pop
+    if self.list.previous
+      elt = self.list.previous
+      self.list.previous = self.list.previous.previous
+    else
+      elt = self.list
+      self.list = nil
+    end
+    elt.datum
+  end
+
+  def shift
+    if self.list
+      if self.list.next
+        if self.list.next != self.list.previous
+          self.list.next.previous = self.list.previous
+        else
+          self.list.next.next = nil
+        end
+      end
+      result = self.list.datum
+      self.list = self.list.next
+      result
+    end
+  end
+
+  def unshift(datum)
+    elt = Element.new(datum, nil)
+    if self.list && self.list.previous
+      elt.previous = self.list.previous
+    else
+      elt.previous = self.list
+    end
+    elt.next = self.list
+    self.list = elt
+  end
 end
--- simple-lists/simple_linked_list.rb
+++ simple-lists/simple_linked_list.rb
@@ -1,3 +1,59 @@
 class Element
+  attr_reader :datum, :next, :previous
 
+  def initialize(datum, nxt)
+    @datum = datum
+    self.next = nxt
+  end
+
+  def next=(elt)
+    @next = elt
+    if (elt && elt.previous != self)
+      elt.previous = self
+    end
+  end
+
+  def previous=(elt)
+    @previous = elt
+    if (elt && elt.next != self)
+      elt.next = self
+    end
+  end
+
+  def to_a
+    self.class.to_a(self)
+  end
+
+  def reverse
+    current = self
+    elt = Element.new(self.datum, nil)
+    while current.next
+      current = current.next
+      elt = Element.new(current.datum, elt)
+    end
+    elt
+  end
+
+  def self.to_a(node)
+    arr = []
+    current = node
+    while current
+      arr << current.datum
+      current = current.next
+    end
+    arr
+  end
+
+  def self.from_a(arr)
+    arr = arr.to_a
+    if arr.length > 0
+      last = Element.new(arr.pop, nil)
+      arr.reverse_each { |elt|
+        last = Element.new(elt, last)
+      }
+      last
+    else
+      nil
+    end
+  end
 end
